e5a4a6078e37289071653e5bc46679130d9c099a
Jaswanth <63146202+jaswanthpallavolu@users.noreply.github.com>
2021-02-09 12:50:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+
+app.use(express.static(__dirname + '/pages'));
+
+app.get('', (req,res)=>{
+    res.sendFile(__dirname + '/pages/index.html')
+})
+
+let port = process.env.PORT || 2000;
+app.listen(port, ()=> console.log('port at '+port+'.......'))
